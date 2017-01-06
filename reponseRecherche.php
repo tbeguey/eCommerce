f374f43c8756d81a19a060c32cd24b1dca123f2f
root <root@pc59.home>
2017-01-06 17:47:48 +0100

    Bug du push d'avant corrigé

--- reponseRecherche.php
+++ reponseRecherche.php
@@ -10,12 +10,18 @@
     $pdodsn = "$driver:Server=$host;Database=$nomDb";
     // Connexion PDO
     $pdo = new PDO($pdodsn, $user, $password);
-    $requestMusician = "Select Nom_Musicien, Prénom_Musicien from Musicien Where Nom_Musicien Like '" . $search ."%'";
+    $requestMusician = "Select Nom_Musicien, Prénom_Musicien from Musicien inner join Composer on Musicien.Code_Musicien = Composer.Code_Musicien"
+        . " inner join Oeuvre on Composer.Code_Oeuvre = Oeuvre.Code_Oeuvre where Nom_Musicien Like :name";
     $requestWork = "Select Titre_Oeuvre from Oeuvre where Titre_Oeuvre Like '" .$search ."%'";
-    foreach ($pdo->query($requete) as $row) 
-    {
-        echo 'Nom / Prénom : ' . $row[utf8_decode('Nom_Musicien')]. " " . $row[utf8_decode('Prénom_Musicien')]. "<br>";
-    }
+    $stmt = $pdo->prepare($requestMusician);
+    $stmt->execute(array('name' => $search . '%'));
+    echo '<h1> Musiciens dont le nom commence par ' . $search . ' : </h1>';
+    while ($row = $stmt->fetch()) {
+    echo "<a href='oeuvre.php?Code=" . $row['Code_Musicien'] . "'/>";
+    echo $row['Nom_Musicien'];
+    echo "<img src='image.php?Code=" . $row['Code_Musicien'] . "'/>";
+    echo "<br>";
+}
     foreach ($pdo->query($requestWork) as $row) 
     {
         echo 'Titre_Oeuvre : ' . $row[utf8_decode('Titre_Oeuvre')]."<br>";
