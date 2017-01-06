85110eb4c3f1f1756259d9fbe1159f37ff548949
root <root@pc59.home>
2017-01-06 17:09:55 +0100

    Ajout des oeuvres dans la recherche

--- reponseRecherche.php
+++ reponseRecherche.php
@@ -10,10 +10,15 @@
     $pdodsn = "$driver:Server=$host;Database=$nomDb";
     // Connexion PDO
     $pdo = new PDO($pdodsn, $user, $password);
-    $requete = "Select Nom_Musicien, Prénom_Musicien from Musicien Where Nom_Musicien Like '" . $search "%'";
-    foreach ($pdo->query($requete) as $row) 
+    $requestMusician = "Select Nom_Musicien, Prénom_Musicien from Musicien Where Nom_Musicien Like '" . $search "%'";
+    $requestWork = "Select Titre_Oeuvre from Oeuvre where Titre_Oeuvre Like '" .$search "%'";
+    foreach ($pdo->query($requestMusician) as $row) 
     {
         echo 'Nom / Prénom : ' . $row[utf8_decode('Nom_Musicien')]. " " . $row[utf8_decode('Prénom_Musicien')]. "<br>";
+    }
+        foreach ($pdo->query($requestWork) as $row) 
+    {
+        echo 'Titre_Oeuvre : ' . $row[utf8_decode('Titre_Oeuvre')]."<br>";
     }
     $pdo = null;
 ?>
