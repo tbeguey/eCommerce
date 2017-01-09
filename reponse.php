2ac5b1a7ea6532350485cdf02cdbba43b7258e7a
root <root@pc59.home>
2017-01-09 15:21:48 +0100

    Confirmation mot de passe - A TESTER

--- reponse.php
+++ reponse.php
@@ -1,29 +1,27 @@
 <?php
-$timeUntExpire = time()+60;
-setcookie($login, $value, $timeUntExpire);
 $dbh = new PDO("sqlsrv:Server=INFO-SIMPLET;Database=Classique_Web", "ETD", "ETD");
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $login = $_POST['login'];
 $password = $_POST['password'];
+$confirm = $_POST['confirm'];
 $query = "INSERT INTO Abonné (Nom_Abonné, Prénom_Abonné, Login, Password) VALUES (:nom,:prenom,:log,:pass)";
 $stmt = $dbh->prepare($query);
-$stmt->execute(array(':nom' => $nom,
+if($password == $confirm){
+	$stmt->execute(array(':nom' => $nom,
     ':prenom' => $prenom,
     ':log' => $login,
     ':pass' => $password));
 echo "Insertion effectué";
 echo "<br>";
-
+}
+else{
+	echo "Mot de passe différent";
+}
 echo "Bienvenue ".$prenom;
 echo "<br>";
 echo "Que voulez vous faire ? : ";
 echo "<br>";
 echo "parametrer mon compte";
 echo "<a href=\"index.html\"> Retour au formulaire </a>";
-
-
-
-
-
 ?>
