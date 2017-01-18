cedcea031238e8bf8281547815544462bf01ead9
Yamin Tricha <ytricha@info-staxx.iut.bx1>
2017-01-18 17:49:52 +0100

    mod front-end

--- reponseConnexion.php
+++ reponseConnexion.php
@@ -6,17 +6,12 @@
     $queryCount = "Select Count(*) from Abonné where Login ='".$Login."' and Password ='".$Password."'";
     $query = "Select Nom_Abonné from Abonné where Login ='".$Login."' and Password ='".$Password."'";
     $stmt = $dbh->query($queryCount);
-    //echo "<h1> " . $stmt->fetchColumn() . "</h1>";
     if($stmt->fetchColumn() > 0){ //utilisateur enregistré avec mot de passe correct
-        echo "<p>if</p>";
         session_start();
         foreach ($dbh->query($query) as $row){
-            echo "her";
             $_SESSION["NOM_USER"] = $row[0];
-            echo "here";
         }
-        echo "hereeeee";
-        header("Location: index.html");        
+        header("Location:index.html");
     }
     //else{//Mot de passe (et/ou login) incorrect : rejet de l'utilisateur
     //    header("Location: connexion.php");
