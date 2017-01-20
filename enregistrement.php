e077578051ab5b3f7358eb75b36e375a53c8ae92
root <root@pc59.home>
2017-01-20 09:48:36 +0100

    Ajout durée des enregistrements

--- enregistrement.php
+++ enregistrement.php
@@ -18,12 +18,13 @@ $password = 'ETD';
 $pdodsn = "$driver:Server=$host;Database=$nomDb";
 // Connexion PDO
 $pdo = new PDO($pdodsn, $user, $password);
-$request = "Select DISTINCT Enregistrement.Titre, Enregistrement.Code_Morceau From Enregistrement
+$request = "Select DISTINCT Enregistrement.Titre, Enregistrement.Code_Morceau, Enregistrement.Durée From Enregistrement
             Where Enregistrement.Code_Morceau=" . $_GET['Code'];
 echo "<ul>";
 foreach ($pdo->query($request) as $row) {
     echo "<li>";
     echo $row[utf8_decode('Titre')];
+    echo $row[utf8_decode('Durée')];
     echo "<audio preload=auto src='extrait.php?Code=" . $row['Code_Morceau'] . "' controls></audio>";
     echo '<form method="post" action="achat.php">';
     echo '<input name="Acheter" type="submit" value="Ajouter au panier">';
