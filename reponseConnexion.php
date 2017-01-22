b568965df2d586488e499aa8b9e5885a5f6759b2
root <root@pc59.home>
2017-01-22 20:35:41 +0100

    test connected

--- reponseConnexion.php
+++ reponseConnexion.php
@@ -1,8 +1,7 @@
 <?php
     $Password = $_REQUEST["Password"];
     $Login=$_REQUEST["Login"];
-            var_dump($Password);
-        var_dump($Login);
+    
     $dbh = new PDO("sqlsrv:Server=INFO-SIMPLET;Database=Classique_Web", "ETD", "ETD");
     // recherche si l'utilisateur est enregistré et possède le bon mot de passe
     $queryCount = "Select Count(*) from Abonné where Login ='".$Login."' and Password ='".$Password."'";
